26c69cc0e449d127d355b7ca7dae593bf6ef2167
YaronPlo <Yaroon15@gmail.com>
2019-12-21 20:18:19 +0200

    Writing ctor for person

--- Group10/Person.cpp
+++ Group10/Person.cpp
@@ -7,3 +7,13 @@ Person::Person()
 	this->id = 123456789;
 	this->age = 20;
 }
+
+Person::Person(string _name, long int _id, int _age)
+{
+	if (_name.length() > 10)
+		_name.erase(10);
+
+	this->name = _name;
+	this->id = _id;
+	this->age = _age;
+}
